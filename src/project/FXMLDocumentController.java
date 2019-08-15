3bdf07bb30261552b0f87b647373f1c517be21b8
lansiquo <lansiquo@sheridancollege.ca>
2019-08-14 23:13:23 -0400

    Robin's commit

--- src/project/FXMLDocumentController.java
+++ src/project/FXMLDocumentController.java
@@ -8,9 +8,17 @@ package project;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -18,18 +26,21 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.AnchorPane;
 
 /**
  *
  * @author bilaa
  */
-public class FXMLDocumentController implements Initializable {
+public class FXMLDocumentController implements EventHandler, Initializable {
 
     @FXML
-    private TextField txtID, txtAddress, txtPin, txtName, txtSearch;
+    private TextField txtName, txtID, txtAddress, txtPin, txtSearch;
 
     @FXML
     private TableView<Account> tblAccount;
@@ -44,11 +55,23 @@ public class FXMLDocumentController implements Initializable {
     private TableColumn<Account, AccountType> tblType;
 
     @FXML
+    private TableColumn<Account, String> tblID;
+
+    @FXML
     private ComboBox<AccountType> cmbType;
 
-    // List to be displayed on the TableView
-    private ObservableList<Account> accountList = FXCollections.observableArrayList();
-    
+    private IntegerProperty index = new SimpleIntegerProperty();
+
+    // list to display on tableview
+    private ObservableList<Account> accountList = FXCollections.observableArrayList(
+            new Account("Robin Lansiquot", "1234", "150 Cindy Street", "9898", AccountType.SAVINGS),
+            new Account("Jimmy John", "4321", "250 Cindy Street", "9898", AccountType.CHEQUEINGS),
+            new Account("Robin Lansiquot", "1234", "150 Cindy Street", "9898", AccountType.SAVINGS),
+            new Account("Jimmy John", "4321", "250 Cindy Street", "9898", AccountType.CHEQUEINGS),
+            new Account("Robin Lansiquot", "1234", "150 Cindy Street", "9898", AccountType.SAVINGS),
+            new Account("Jimmy John", "4321", "250 Cindy Street", "9898", AccountType.CHEQUEINGS)
+    );
+
     @FXML
     private AnchorPane main, first;
     @FXML
@@ -62,7 +85,7 @@ public class FXMLDocumentController implements Initializable {
         if (txtName.getText().matches(validName) && txtAddress.getText().matches(validAddress)) {
             if (txtAddress.getText().matches(validAddress)) {
                 // Create new Business with user inputted values
-                Account account = new Account(txtName.getText(), Integer.parseInt(txtID.getText()), txtAddress.getText(), txtPin.getText(), cmbType.getValue());
+                Account account = new Account(txtName.getText(), txtID.getText(), txtAddress.getText(), txtPin.getText(), cmbType.getValue());
                 // Add to the account list
                 accountList.add(account);
                 // Clear TextFields and ComboBoxes after user clicks Add button
@@ -81,18 +104,23 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void remove(ActionEvent event) {
-
+        //index of accounts on the tableview list
+        int i = index.get();
+        //if index is greater than -1, remove account at the index, also clear 
+        if (i>-1) {
+            accountList.remove(i);
+            tblAccount.getSelectionModel().clearSelection();
+        }
     }
 
     @FXML
     private void transaction(ActionEvent event) throws IOException {
 
-        
         TabPane pane = FXMLLoader.load(getClass().getResource("FXMLTransaction.fxml"));
         main.getChildren().setAll(pane);
-        
+
     }
-    
+
     public void alertError(String text) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
@@ -103,7 +131,93 @@ public class FXMLDocumentController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
+//        prompt text and dropdown menu
+//        cmbType.setPromptText("Account Type");
+//        cmbType.getItems().setAll(AccountType.values());
+        
+        
+        //searching through any of the 4 columns
+        txtSearch.textProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable o) {
+                //if the user did not search, then display list of accounts
+                if (txtSearch.textProperty().get().isEmpty()) {
+                    tblAccount.setItems(accountList);
+                    return;
+                }
+                ObservableList<Account> items = FXCollections.observableArrayList();
+                ObservableList<TableColumn<Account, ?>> columns = tblAccount.getColumns();
+                //search through rows
+                for (int i = 0; i < accountList.size(); i++) {
+                    for (int j = 0; j < columns.size(); j++) {
+                        TableColumn column = columns.get(j);
+                        String cellValue = column.getCellData(accountList.get(i)).toString();
+                        cellValue = cellValue.toLowerCase();
+                        //if no match then leave row in list
+                        if (cellValue.contains(txtSearch.textProperty().get().toLowerCase())) {
+                            items.add(accountList.get(i));
+                            break;
+                        }
+                    }
+                }
+                tblAccount.setItems(items);
+            }
+        }
+        );
+
+        //Setting the columns
+        tblName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        tblID.setCellValueFactory(new PropertyValueFactory<>("ID"));
+        tblAddress.setCellValueFactory(new PropertyValueFactory<>("Address"));
+        tblType.setCellValueFactory(new PropertyValueFactory<>("Account Type"));
+        tblAccount.setItems(accountList);
+
+        //allowing the tableview to be editable
+        tblAccount.setEditable(true);
+
+//        Updates the name cell with input from the user
+        tblName.setCellFactory(TextFieldTableCell.forTableColumn());
+        tblName.setOnEditCommit(new EventHandler<CellEditEvent<Account, String>>() {
+            @Override
+            public void handle(CellEditEvent<Account, String> event) {
+                ((Account) event.getTableView().getItems().get(event.getTablePosition().getRow())).setName(event.getNewValue());
+            }
+        });
+
+//        Updates ID cell with input from the user
+        tblID.setCellFactory(TextFieldTableCell.forTableColumn());
+        tblID.setOnEditCommit(new EventHandler<CellEditEvent<Account, String>>() {
+            @Override
+            public void handle(CellEditEvent<Account, String> event) {
+                ((Account) event.getTableView().getItems().get(event.getTablePosition().getRow())).setAddress(event.getNewValue());
+            }
+        });
+////        
+//        Updates the address cell with input from the user
+        tblAddress.setCellFactory(TextFieldTableCell.forTableColumn());
+        tblAddress.setOnEditCommit(new EventHandler<CellEditEvent<Account, String>>() {
+            @Override
+            public void handle(CellEditEvent<Account, String> event) {
+                ((Account) event.getTableView().getItems().get(event.getTablePosition().getRow())).setAddress(event.getNewValue());
+            }
+        });
+
+        //the tableview list index is set to -1
+        index.set(-1);
+        //updates table with input from the user
+        tblAccount.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Object>() {
+            @Override
+            public void changed(ObservableValue<? extends Object> observable, Object oldValue, Object newValue) {
+                index.set(accountList.indexOf(newValue));
+            }
+        });
+
+    }
+
+    @Override
+    public void handle(Event event) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
